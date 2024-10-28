c3206aef1c5bc84cbf4060b24f3b1c67add7942c
Elmirbek Surkeev <elmirsurkeev@gmail.com>
2024-10-28 17:05:43 +0600

    added true manipulation with Time

--- src/Action.java
+++ src/Action.java
@@ -6,10 +6,7 @@ public class Action {
     public static void main() {
         Random rnd = new Random();
         List<Car> allCars = new ArrayList<>();
-
-        // Форматирование даты с помощью DateTimeFormatter
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
         Map<String, Car> inParking = new HashMap<>();
 
         for (int i = 1; i <= 200; i++) {
@@ -25,10 +22,19 @@ public class Action {
             if (probability == 3) {
                 car.changeState("onParking");
                 inParking.put(now.format(formatter), car);
-                System.out.println(inParking);
+          //      System.out.println(inParking);
             }
         }
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime end = now.plusDays(30);
+        LocalDateTime current = now;
 
-        allCars.forEach(System.out::println);
+
+        while(current.isBefore(end) || current.isEqual(end)) {
+            System.out.println(current.format(formatter));
+            current = current.plusMinutes(5);
+        }
+
+        //allCars.forEach(System.out::println);
     }
 }
