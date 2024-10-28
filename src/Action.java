7931627beb8dedbb76818a9ceb3d33655f182aec
Elmirbek Surkeev <elmirsurkeev@gmail.com>
2024-10-28 17:44:52 +0600

    update system with cheking data with car in route, in parking place

--- src/Action.java
+++ src/Action.java
@@ -7,34 +7,47 @@ public class Action {
         Random rnd = new Random();
         List<Car> allCars = new ArrayList<>();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        Map<String, Car> inParking = new HashMap<>();
 
+        // Автомобили, которые едут по городу
         for (int i = 1; i <= 200; i++) {
-            LocalDateTime now = LocalDateTime.now();
-            int probability = rnd.nextInt(100);
-
-            // Автомобили, которые едут по городу
             String logo = Logo.random().toString();
             Car car = new Car(i, logo, "onRoute", "124");
             allCars.add(car);
-
-            // На парковке с вероятностью 3%
-            if (probability == 3) {
-                car.changeState("onParking");
-                inParking.put(now.format(formatter), car);
-          //      System.out.println(inParking);
-            }
         }
+
+        // Логика времени
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime end = now.plusDays(30);
         LocalDateTime current = now;
 
-
-        while(current.isBefore(end) || current.isEqual(end)) {
-            System.out.println(current.format(formatter));
+        // Хранение информации о парковке
+        Map<String, Car> inParking = new HashMap<>();
+        while (current.isBefore(end) || current.isEqual(end)) {
             current = current.plusMinutes(5);
+
+            // Процентная вероятность
+            int probability = rnd.nextInt(34);
+            int probabilityOut = rnd.nextInt(34);
+
+            // На парковке с вероятностью 3%
+            if (probability < 3) { // Если число в диапазоне 0, 1, или 2
+                Car car = allCars.get(rnd.nextInt(allCars.size()));
+                car.changeState("onParking");
+                inParking.put(current.format(formatter), car);
+                allCars.remove(car);
+                System.out.println("Парковка: " + car + " время: " + current.format(formatter));
+            }
+            if (probabilityOut <3 && !inParking.isEmpty()){
+                List<String> key = new ArrayList<>(inParking.keySet());
+                String randomKey = key.get(rnd.nextInt(key.size()));
+
+                Car car = inParking.get(randomKey);
+                car.changeState("onRoute");
+                allCars.add(car);
+                inParking.remove(randomKey);
+                System.out.println("Возвращается на маршрут: " + car + " время: " + current.format(formatter));
+            }
         }
 
-        //allCars.forEach(System.out::println);
     }
 }
